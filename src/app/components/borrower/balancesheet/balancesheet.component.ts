58a0bea0c26a1fec4516a8f92698d68c519b1407
perfextion <taransalh86@gmail.com>
2018-06-08 23:41:24 +0530

    Editing on Balance Sheet Items

--- src/app/components/borrower/balancesheet/balancesheet.component.ts
+++ src/app/components/borrower/balancesheet/balancesheet.component.ts
@@ -16,61 +16,132 @@ import { LoggingService } from '../../../services/Logs/logging.service';
 })
 export class BalancesheetComponent implements OnInit {
 
-  private localloanobject:loan_model=new loan_model();
-  public allDataFetched=false;  
-  public editarray=[];
-  public syncenabled=false;
-  constructor(public localstorageservice:LocalStorageService,
-  public loanserviceworker:LoancalculationWorker,
-  public borrowerservice:BorrowerapiService,
-  private toaster: ToastsManager,
-  public logging:LoggingService
-  ) { }
+  private localloanobject: loan_model = new loan_model();
+  public syncenabled = false;
+  // Aggrid
+  public rowData = [];
+  public pinnedBottomRowData = [];
+  private getRowClass;
+  private gridApi;
+  private columnApi;
+   //region Ag grid Configuration
+
+   columnDefs = [
+    { headerName: 'Financials', field: 'Financials' },
+    { headerName: 'Assets', field: 'Assets',editable:true },
+    { headerName: 'Discount', field: 'Discount',editable:true },
+    { headerName: 'AdjValue', field: 'AdjValue' },
+    { headerName: 'Debt', field: 'Debt',editable:true },
+    { headerName: 'Discounted NW', field: 'DiscountedNW' }
+  ];
+
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.columnApi = params.columnApi;
+    this.gridApi.sizeColumnsToFit();
+    window.onresize = () => {
+        this.gridApi.sizeColumnsToFit();
+    }
+}
+  //End here
+  // Aggrid ends
+  constructor(public localstorageservice: LocalStorageService,
+    public loanserviceworker: LoancalculationWorker,
+    public borrowerservice: BorrowerapiService,
+    private toaster: ToastsManager,
+    public logging: LoggingService
+  ) { 
+
+    this.getRowClass = function(params) {
+      debugger
+      if (params.node.rowPinned) {
+        debugger
+        return  'ag-aggregate-row';
+      }
+    };
+  }
+
   ngOnInit() {
-    this.localstorageservice.observe(environment.loankey).subscribe(res=>{
-      this.logging.checkandcreatelog(1,'BalanceSheet',"LocalStorage updated");
-      this.localloanobject=res;
-      this.allDataFetched=true;
+    this.localstorageservice.observe(environment.loankey).subscribe(res => {
+      this.logging.checkandcreatelog(1, 'BalanceSheet', "LocalStorage updated");
+      this.localloanobject = res;
+      this.rowData=[];
+      this.pinnedBottomRowData=[];
+      this.prepareviewmodel();
     })
     this.getdataforgrid();
+    this.prepareviewmodel();
   }
-  getdataforgrid(){
-    let obj:any=this.localstorageservice.retrieve(environment.loankey);
-    this.logging.checkandcreatelog(1,'BalanceSheet',"LocalStorage retrieved");
-    if(obj!=null && obj!=undefined)
-    {
-    this.localloanobject=obj;
-    this.allDataFetched=true;
+  getdataforgrid() {
+    let obj: any = this.localstorageservice.retrieve(environment.loankey);
+    this.logging.checkandcreatelog(1, 'BalanceSheet', "LocalStorage retrieved");
+    if (obj != null && obj != undefined) {
+      this.localloanobject = obj;
     }
   }
 
-  startediting(value:string){
-  this.editarray=[];
-  this.editarray[value]=true;
-  }
-  valueupdate(value:any,key:string){
-    debugger
-    this.editarray[key]=false;
-    if(value==""){
-      value="0";
+  // Ag grid Editing Event
+  celleditingstopped(event: any) {
+    
+    var financetype=event.data.Financials;
+    if(financetype=="Long Term"){
+      financetype="Fixed";
     }
-    this.localloanobject.Borrower[key]=parseInt(value);
-    this.logging.checkandcreatelog(3,'BalanceSheet',"Field Edited -"+key);
-    this.loanserviceworker.performcalculationonloanobject(this.localloanobject);
-    this.syncenabled=true;
+  var property="";
+  if(event.colDef.field=="Discount")
+  {
+    property="Borrower_"+financetype+"_Assets_Disc";
+  }
+  if(event.colDef.field=="Assets")
+  {
+  property="Borrower_"+financetype+"_"+event.colDef.field;
+  }
+  if(event.colDef.field=="Debt")
+  {
+    property="Borrower_"+financetype+"_Liabilities";
+  }
+  this.localloanobject.Borrower[property]=parseFloat(event.value);
+  this.logging.checkandcreatelog(3,'BalanceSheet',"Field Edited -"+property);
+  this.loanserviceworker.performcalculationonloanobject(this.localloanobject);
   }
 
-   synctoDb(){
-    var obj=modelparserfordb(this.localloanobject.Borrower);
-    this.borrowerservice.saveupdateborrower(obj).subscribe(res=>{
-      this.logging.checkandcreatelog(3,'BalanceSheet',"Code Synced to DB with ResCode "+res.ResCode);
-      if(res.ResCode==1){
+  //Aggrid Data Preperation 
+  prepareviewmodel() {
+    //prepare three rows here
+      //1st Current Financial Row 
+      var currentobj={Financials:'Current',Assets:this.localloanobject.Borrower.Borrower_Current_Assets,Discount:this.localloanobject.Borrower.Borrower_Current_Assets_Disc,
+      AdjValue:this.localloanobject.Borrower.FC_Borrower_Current_Adjvalue,Debt:this.localloanobject.Borrower.Borrower_Current_Liabilities,DiscountedNW:this.localloanobject.Borrower.FC_Borrower_Current_Discvalue}
+      this.rowData.push(currentobj);
+
+       //1st Intermediate Financial Row 
+      var Intermediateobj={Financials:'Intermediate',Assets:this.localloanobject.Borrower.Borrower_Intermediate_Assets,Discount:this.localloanobject.Borrower.Borrower_Intermediate_Assets_Disc,
+      AdjValue:this.localloanobject.Borrower.FC_Borrower_Intermediate_Adjvalue,Debt:this.localloanobject.Borrower.Borrower_Intermediate_Liabilities,DiscountedNW:this.localloanobject.Borrower.FC_Borrower_Intermediate_Discvalue}
+      this.rowData.push(Intermediateobj)
+
+      
+       //1st LongTerm Financial Row 
+       var LongTermobj={Financials:'Long Term',Assets:this.localloanobject.Borrower.Borrower_Fixed_Assets,Discount:this.localloanobject.Borrower.Borrower_Fixed_Assets_Disc,
+       AdjValue:this.localloanobject.Borrower.FC_Borrower_Fixed_Adjvalue,Debt:this.localloanobject.Borrower.Borrower_Fixed_Liabilities,DiscountedNW:this.localloanobject.Borrower.FC_Borrower_Fixed_Discvalue}
+       this.rowData.push(LongTermobj)
+
+       //Last Aggregate Row
+       var Aggregateobj={Financials:'Total',Assets:this.localloanobject.Borrower.FC_Borrower_TotalAssets,Discount:'',
+       AdjValue:this.localloanobject.Borrower.FC_Borrower_Total_Adjvalue,Debt:this.localloanobject.Borrower.FC_Borrower_TotalDebt,DiscountedNW:this.localloanobject.Borrower.FC_Borrower_Total_Discvalue}
+       this.pinnedBottomRowData.push(Aggregateobj);
+
+  }
+  //ends here
+  synctoDb() {
+    var obj = modelparserfordb(this.localloanobject.Borrower);
+    this.borrowerservice.saveupdateborrower(obj).subscribe(res => {
+      this.logging.checkandcreatelog(3, 'BalanceSheet', "Code Synced to DB with ResCode " + res.ResCode);
+      if (res.ResCode == 1) {
         this.toaster.success("Object Synced Successfully");
       }
-      else{
+      else {
         this.toaster.error("Error Encountered while Syncing");
-       }
+      }
     });
-   }
+  }
 
 }
