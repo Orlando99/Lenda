ab955e6fa200c7ce24453424c4880022c8d78b51
Varun Goel <varun@backbase.com>
2018-07-05 23:02:59 +0200

    Implemented footer with version information

--- src/app/shared/layout/footer.component.ts
+++ src/app/shared/layout/footer.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'ngx-webstorage';
+
+import { environment } from '../../../environments/environment';
+import { versions } from '../../versions';
+import { ReferenceService } from '../../services/reference/reference.service';
 
 @Component({
   selector: 'layout-footer',
-  templateUrl: './footer.component.html'
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
-  constructor(){}
+export class FooterComponent implements OnInit {
+  public apiurl = environment.apiUrl;
+  public git = versions.revision;
+  public databaseName: string = "";
+
+  constructor(public localst: LocalStorageService,
+  public referenceService: ReferenceService) { }
+
+  ngOnInit() {
+    this.getReferenceData();
+  }
+
+  getReferenceData() {
+    this.referenceService.getreferencedata().subscribe(res => {
+      this.localst.store(environment.referencedatakey, res.Data);
+      this.databaseName = res.Data.Databasename;
+    })
+  }
 }
