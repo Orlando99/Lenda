d03c2bcb03d7aeb4ba827ff9c11a329d6c600c9e
Varun Goel <varun@backbase.com>
2018-07-24 17:38:30 +0200

    Dynamic name connection from local storage

--- src/app/components/summary/charts-visualization/company-info/company-info.component.ts
+++ src/app/components/summary/charts-visualization/company-info/company-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { LocalStorageService } from 'ngx-webstorage';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-company-info',
@@ -7,12 +9,27 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CompanyInfoComponent implements OnInit {
   @Input() viewMode:number;
+  private info = {
+    firstName: '',
+    lastName: '',
+    loanFullId: ''
+  }
 
   public starsCount: number = 3.5;
 
-  constructor() { }
+  constructor(
+    private localStorageService: LocalStorageService
+  ) { }
 
   ngOnInit() {
+    this.getCompanyInfo();
   }
 
+  getCompanyInfo() {
+    let loanBudgets = this.localStorageService.retrieve(environment.loankey_copy);
+    let loanMaster = loanBudgets.LoanMaster[0];
+    this.info.firstName = loanMaster.Borrower_First_Name;
+    this.info.lastName = loanMaster.Borrower_Last_Name;
+    this.info.loanFullId = loanMaster.Loan_Full_ID;
+  }
 }
