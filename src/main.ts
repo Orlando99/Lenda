6edb82f7058812ba2ed6aac19f9af9472f04fa22
alexey <alex06150210@gmail.com>
2018-07-25 08:40:49 +1000

    fix some error

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment.prod';
+import {LicenseManager} from "ag-grid-enterprise";
+
 import 'hammerjs';
+LicenseManager.setLicenseKey("Evaluation_License_Valid_Until__9_September_2018__MTUzNjQ0NzYwMDAwMA==712c48d48d0a3ec85f3243b1295999ec");
 
 if (environment.production) {
   enableProdMode();
