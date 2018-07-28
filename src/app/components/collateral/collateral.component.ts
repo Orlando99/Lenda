0c43e1a3fae29a48d69004143d620ac05ba47878
Varun Goel <varun@backbase.com>
2018-07-28 09:00:49 +0200

    Added the code for selection of tables to be displayed

--- src/app/components/collateral/collateral.component.ts
+++ src/app/components/collateral/collateral.component.ts
@@ -9,6 +9,53 @@ import { LiveStockService } from './livestock/livestock.service';
   providers: [FsaService, LiveStockService]
 })
 export class CollateralComponent implements OnInit {
+  public pageSettings = {
+    lineHolder: {
+      isActive: true,
+      isDisabled: true
+    },
+    buyer: {
+      isActive: true,
+      isDisabled: true
+    },
+    guarantor: {
+      isActive: true,
+      isDisabled: true
+    },
+    fsa: {
+      isActive: true,
+      isDisabled: true
+    },
+    livestock: {
+      isActive: false,
+      isDisabled: false
+    },
+    storedCrop: {
+      isActive: false,
+      isDisabled: false
+    },
+    equipment: {
+      isActive: false,
+      isDisabled: false
+    },
+    realestate: {
+      isActive: false,
+      isDisabled: false
+    },
+    other: {
+      isActive: false,
+      isDisabled: false
+    },
+    contacts: {
+      isActive: true,
+      isDisabled: true
+    },
+    crossCollateral: {
+      isActive: true,
+      isDisabled: true
+    }
+  };
+
   constructor() { }
 
   ngOnInit() {
