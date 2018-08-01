ff71f40121af8611ef4c570a19dbb71f6d9a2811
Sanket Patel <3sanket3@gmail.com>
2018-08-01 18:06:36 +0530

    resolve yield reporst first time load issue

--- src/app/components/summary/yield-report/yield-report.component.ts
+++ src/app/components/summary/yield-report/yield-report.component.ts
@@ -19,27 +19,36 @@ export class YieldReportComponent implements OnInit {
 
   ngOnInit() {    
 
-    this.cropYear = this.localstorageservice.retrieve(environment.loankey).LoanMaster[0] != null ? this.localstorageservice.retrieve(environment.loankey).LoanMaster[0].Crop_Year : 0;
+    //this.cropYear = this.localstorageservice.retrieve(environment.loankey).LoanMaster[0] != null ? this.localstorageservice.retrieve(environment.loankey).LoanMaster[0].Crop_Year : 0;
 
-    for(let i=1; i<8;i++){
-      this.years.push(this.cropYear-i);
-    };
+    
     this.localstorageservice.observe(environment.loankey).subscribe(res => {
       if(res!=undefined && res!=null)
       {
 
-        if(res && res.CropYield){
-          this.cropYield = res.CropYield;
+        if(res && res.CropYield && res.LoanMaster){
+          this.prepareData(res);
         }       
        
       }
     });
   
     let res: any = this.localstorageservice.retrieve(environment.loankey);
-    if(res && res.CropYield){
-      this.cropYield = res.CropYield;
+
+    if(res && res.CropYield && res.LoanMaster){
+      this.prepareData(res);
     }
   }
 
 
+
+  private prepareData(res: any) {
+    this.cropYield = res.CropYield;
+    this.cropYear = res.LoanMaster[0].Crop_Year;
+    this.years = [];
+    for (let i = 1; i < 8; i++) {
+      this.years.push(this.cropYear - i);
+    }
+    ;
+  }
 }
