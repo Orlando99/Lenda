bd3616a120f41d8348a05975f6779c0e497d058b
Varun Goel <varun@backbase.com>
2018-08-04 22:57:05 +0200

    Added configuration for collateral settings

--- src/app/components/collateral/collateral-types.model.ts
+++ src/app/components/collateral/collateral-types.model.ts
@@ -1,9 +1,62 @@
-enum CollateralTypes {
-  equipment = 'EQP',
-  fsa = 'FSA',
-  livestock = 'LSK',
-  marketingContracts = '',
-  others = 'OTR',
-  realEstate = 'RET',
-  storedCrop = 'SCP'
-};
+export default {
+  lineHolder: {
+    key: 'TBD1',
+    source: '',
+    sourceKey: ''
+  },
+  buyer: {
+    key: 'BUY',
+    source: 'Association',
+    sourceKey: 'Assoc_Type_Code'
+  },
+  guarantor: {
+    key: 'TBD2',
+    source: '',
+    sourceKey: 'TBD'
+  },
+  collaterlData: {
+    key: 'TBD3',
+    source: 'LoanCollateral',
+    sourceKey: 'TBD'
+  },
+  fsa: {
+    key: 'FSA',
+    source: 'LoanCollateral',
+    sourceKey: 'Collateral_Category_Code'
+  },
+  livestock: {
+    key: 'LSK',
+    source: 'LoanCollateral',
+    sourceKey: 'Collateral_Category_Code'
+  },
+  storedCrop: {
+    key: 'SCP',
+    source: 'LoanCollateral',
+    sourceKey: 'Collateral_Category_Code'
+  },
+  equipment: {
+    key: 'EQP',
+    source: 'LoanCollateral',
+    sourceKey: 'Collateral_Category_Code'
+  },
+  realestate: {
+    key: 'RET',
+    source: 'LoanCollateral',
+    sourceKey: 'Collateral_Category_Code'
+  },
+  other: {
+    key: 'OTR',
+    source: 'LoanCollateral',
+    sourceKey: 'Collateral_Category_Code'
+  },
+  contacts: {
+    key: 'TBD4',
+    source: '',
+    sourceKey: 'TBD'
+  },
+  marketingContracts: {
+    key: '',
+    source: 'LoanMarketingContracts',
+    sourceKey: ''
+  }
+}
--- src/app/components/collateral/collateral.component.ts
+++ src/app/components/collateral/collateral.component.ts
@@ -4,6 +4,8 @@ import { LiveStockService } from './livestock/livestock.service';
 import { FormControl } from '@angular/forms';
 import { LocalStorageService } from 'ngx-webstorage';
 import { environment } from '../../../environments/environment.prod';
+import CollteralSettings from './collateral-types.model';
+
 @Component({
   selector: 'app-collateral',
   templateUrl: './collateral.component.html',
@@ -14,6 +16,8 @@ import { environment } from '../../../environments/environment.prod';
 export class CollateralComponent implements OnInit {
   categories: any = [];
   loanFullID: string;
+  collateralRows: any = {};
+
   // Default settings of collateral tables
   public pageSettings = {
     lineHolder: {
@@ -72,11 +76,33 @@ export class CollateralComponent implements OnInit {
 
   ngOnInit() {
     this.pageSettings = this.localstorageservice.retrieve(environment.collateralTables) || this.pageSettings;
+    this.getCollateralTypeRows();
   }
 
   onToggleSettings(item) {
     item.isActive = !item.isActive;
     this.localstorageservice.store(environment.collateralTables, this.pageSettings);
+  }
+
+  getCollateralTypeRows() {
+    for (let index in CollteralSettings) {
+      let item = CollteralSettings[index];
+      this.collateralRows[index] = this.getRowsForType(item.key, item.source, item.sourceKey);
+    }
+  }
+
+  getRowsForType(type: string, src: string, srcType: string) {
+    let items = this.localstorageservice.retrieve(environment.loankey)[src];
+    if (!items) {
+      return 0;
+    }
+    if (!srcType) {
+      return items.length;
+    }
+    return items
+      .filter(lc => {
+        return lc[srcType] === type
+      }).length;
   }
 }
 
