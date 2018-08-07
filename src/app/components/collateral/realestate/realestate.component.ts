489455c8ab11ef6d01b02dbe0a11988157d81646
Varun Goel <varun@backbase.com>
2018-08-08 00:31:28 +0200

    Completed refactoring for collateral

--- src/app/components/collateral/realestate/realestate.component.ts
+++ src/app/components/collateral/realestate/realestate.component.ts
@@ -6,7 +6,6 @@ import { LoancalculationWorker } from '../../../Workers/calculations/loancalcula
 import { LoggingService } from '../../../services/Logs/logging.service';
 import { CropapiService } from '../../../services/crop/cropapi.service';
 import { getNumericCellEditor } from '../../../Workers/utility/aggrid/numericboxes';
-import { currencyFormatter, insuredFormatter, discFormatter, numberFormatter } from '../../../Workers/utility/aggrid/collateralboxes';
 import { DeleteButtonRenderer } from '../../../aggridcolumns/deletebuttoncolumn';
 import { AlertifyService } from '../../../alertify/alertify.service';
 import { LoanApiService } from '../../../services/loan/loanapi.service';
@@ -50,48 +49,10 @@ export class RealEstateComponent implements OnInit {
     private hostElement: ElementRef,
     public collateralService: CollateralService,
     public realEstateService: RealEstateService) {
-
     this.components = { numericCellEditor: getNumericCellEditor(), alphaNumeric: getAlphaNumericCellEditor() };
     this.refdata = this.localstorageservice.retrieve(environment.referencedatakey);
     this.frameworkcomponents = { selectEditor: SelectEditor, deletecolumn: DeleteButtonRenderer };
-
-
-    this.columnDefs = [
-      { headerName: 'Category', field: 'Collateral_Category_Code', editable: false, width: 100 },
-      { headerName: 'Description', field: 'Collateral_Description', cellEditor: "alphaNumeric", editable: true, width: 120, cellClass: 'editable-color' },
-      { headerName: 'Qty', field: 'Qty', editable: true, cellEditor: "numericCellEditor", valueFormatter: numberFormatter, cellClass: ['editable-color', 'text-right'], width: 90 },
-      { headerName: 'Price', field: 'Price', editable: true, cellEditor: "numericCellEditor", valueFormatter: currencyFormatter, cellClass: ['editable-color', 'text-right'], width: 110 },
-
-      { headerName: 'Mkt Value', field: 'Market_Value', editable: false, cellEditor: "numericCellEditor", valueFormatter: currencyFormatter, cellClass: ['text-right'], width: 130 },
-      { headerName: 'Prior Lien', field: 'Prior_Lien_Amount', editable: true, cellEditor: "numericCellEditor", valueFormatter: currencyFormatter, cellClass: ['editable-color', 'text-right'], width: 130 },
-      { headerName: 'Lienholder', field: 'Lien_Holder', editable: true, width: 120, cellEditor: "alphaNumeric", cellClass: ['editable-color'] },
-      {
-        headerName: 'Net Mkt Value', field: 'Net_Market_Value', editable: false, cellEditor: "numericCellEditor", valueFormatter: currencyFormatter, cellClass: ['text-right'],
-        // valueGetter: function (params) {
-        //   return setNetMktValue(params);}
-      },
-      {
-        headerName: 'Discount %', field: 'Disc_Value', editable: true, cellEditor: "numericCellEditor", valueFormatter: discFormatter, cellStyle: { textAlign: "right" }, width: 110, cellClass: ['editable-color', 'text-right'],
-        pinnedRowCellRenderer: function () { return ' '; }
-      },
-      {
-        headerName: 'Disc Value', field: 'Disc_CEI_Value', editable: false, cellEditor: "numericCellEditor", cellClass: ['text-right'],
-        // valueGetter: function (params) {
-        //   return setDiscValue(params);
-        // },
-        valueFormatter: currencyFormatter
-      },
-      {
-        headerName: 'Insured', field: 'Insured_Flag', editable: true, cellEditor: "selectEditor", width: 100, cellClass: ['editable-color'],
-        cellEditorParams: {
-          values: [{ 'key': 0, 'value': 'No' }, { 'key': 1, 'value': 'Yes' }]
-        }, pinnedRowCellRenderer: function () { return ' '; },
-        valueFormatter: insuredFormatter
-      },
-      { headerName: '', field: 'value', cellRenderer: "deletecolumn", width: 80, pinnedRowCellRenderer: function () { return ' '; } }
-    ];
-
-
+    this.columnDefs = this.realEstateService.getColumnDefs();
     this.context = { componentParent: this };
   }
 
--- src/app/components/collateral/storedcrop/storedcrop.service.ts
+++ src/app/components/collateral/storedcrop/storedcrop.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { loan_model, Loan_Collateral } from '../../../models/loanmodel';
+import { currencyFormatter, insuredFormatter, discFormatter, numberFormatter } from '../../../Workers/utility/aggrid/collateralboxes';
 
 /**
  * Shared service for Stored Crop
@@ -7,6 +8,43 @@ import { loan_model, Loan_Collateral } from '../../../models/loanmodel';
 @Injectable()
 export class StoredCropService {
   constructor() {
+  }
+
+  getColumnDefs() {
+    return [
+      { headerName: 'Category', field: 'Collateral_Category_Code', editable: false, width: 100 },
+      { headerName: 'Description', field: 'Collateral_Description', cellEditor: "alphaNumeric", editable: true, width: 120, cellClass: 'editable-color' },
+      { headerName: 'Qty', field: 'Qty', editable: true, cellEditor: "numericCellEditor", valueFormatter: numberFormatter, cellClass: ['editable-color', 'text-right'], width: 90 },
+      { headerName: 'Price', field: 'Price', editable: true, cellEditor: "numericCellEditor", valueFormatter: currencyFormatter, cellClass: ['editable-color', 'text-right'], width: 110 },
+
+      { headerName: 'Mkt Value', field: 'Market_Value', editable: false, cellEditor: "numericCellEditor", valueFormatter: currencyFormatter, cellClass: ['text-right'], width: 130 },
+      { headerName: 'Prior Lien', field: 'Prior_Lien_Amount', editable: true, cellEditor: "numericCellEditor", valueFormatter: currencyFormatter, cellClass: ['editable-color', 'text-right'], width: 130 },
+      { headerName: 'Lienholder', field: 'Lien_Holder', editable: true, width: 120, cellEditor: "alphaNumeric", cellClass: ['editable-color'] },
+      {
+        headerName: 'Net Mkt Value', field: 'Net_Market_Value', editable: false, cellEditor: "numericCellEditor", valueFormatter: currencyFormatter, cellClass: ['text-right'],
+        // valueGetter: function (params) {
+        //   return setNetMktValue(params);}
+      },
+      {
+        headerName: 'Discount %', field: 'Disc_Value', editable: true, cellEditor: "numericCellEditor", valueFormatter: discFormatter, cellStyle: { textAlign: "right" }, width: 110, cellClass: ['editable-color', 'text-right'],
+        pinnedRowCellRenderer: function () { return ' '; }
+      },
+      {
+        headerName: 'Disc Value', field: 'Disc_CEI_Value', editable: false, cellEditor: "numericCellEditor", cellClass: ['text-right'],
+        // valueGetter: function (params) {
+        //   return setDiscValue(params);
+        // },
+        valueFormatter: currencyFormatter
+      },
+      {
+        headerName: 'Insured', field: 'Insured_Flag', editable: true, cellEditor: "selectEditor", width: 100, cellClass: ['editable-color'],
+        cellEditorParams: {
+          values: [{ 'key': 0, 'value': 'No' }, { 'key': 1, 'value': 'Yes' }]
+        }, pinnedRowCellRenderer: function () { return ' '; },
+        valueFormatter: insuredFormatter
+      },
+      { headerName: '', field: 'value', cellRenderer: "deletecolumn", width: 80, pinnedRowCellRenderer: function () { return ' '; } }
+    ];
   }
 
   computeTotal(input) {
